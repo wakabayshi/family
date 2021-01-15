e2b66f171848626381eae6c93c8cb1909ea0d51b
wakabayashi <ne191198@senshu-u.jp>
2021-01-15 16:37:37 +0000

    Heroku用の設定

--- db/seeds/production/periods.rb
+++ db/seeds/production/periods.rb
@@ -1,5 +1,6 @@
 time=%w(月曜午前 月曜午後 火曜午前 火曜午後 水曜午前 水曜午後 木曜午前 木曜午後 金曜午前 金曜午後)
 names=["Dr1","Dr2","Dr3"]
+d1=Date.new(2020,1,18)
 0.upto(9) do |idx|
   period=Period.create(
     doctor: Doctor.find_by(name: names[idx%3]),
@@ -7,6 +8,22 @@ names=["Dr1","Dr2","Dr3"]
     number: 0,
     maxnumber: 10,
     before: (idx%2==0),
-    date: "2000/#{rand(12)}/#{rand(30)}"
+    date: d1
   )
+  if idx%10==0 || idx%10==1
+    period.date=d1+
+    period.save
+  elsif idx%10==2|| idx%10==3
+    period.date=d1+1
+    period.save
+  elsif idx%10==4|| idx%10==5
+    period.date=d1+2
+    period.save
+  elsif idx%10==6|| idx%10==7
+    period.date=d1+3
+    period.save
+  elsif idx%10==8|| idx%10==9
+    period.date=d1+4
+    period.save
+  end
 end
